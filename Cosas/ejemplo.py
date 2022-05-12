c7b3bfa41cb317adba33c810955f16b3b91c6e20
ionatan <ionatan@gmail.com>
2022-05-12 20:59:47 -0300

    update

---
+++ Cosas/ejemplo.py
@@ -0,0 +1,10 @@
+def hola_curso():
+    print ("Hola DH")
+
+class prueba():
+
+    def __init__(self):
+        self.nombre = "Ioni"
+
+    def imprimir_nombre(self):
+        print (f"Hola, soy{self.nombre}")
---
+++ Cosas/main.py
@@ -0,0 +1,5 @@
+palabra = " Hola mundooooo "
+print (palabra.strip(" o"))
+
+
+
